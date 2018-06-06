a349f735f78fb80137cb935da22475ea1aede282
陈健明 <bluecat000@outlook.com>
2018-06-06 18:48:19 +0800

    import fn

--- es6/import.js
+++ es6/import.js
@@ -1,5 +1,5 @@
 window.global = 1
-import def, {b as a} from './export.js'
+// import def, {b as a} from './export.js'
 // import * as aaaaa from './export.js' //提升，所有代码执行前执行
 // import {fn as func} from './export.js'
 // import {fn, b} from './export.js'
@@ -18,8 +18,15 @@ import def, {b as a} from './export.js'
   // console.log(err)
 // })
 
-console.log(def)
-console.log(a)
+// 动态加载，返回promise 回调函数参数是整个mudule, fn.default
+if (true) {
+
+  import('./export.js')
+    .then((fn) => {
+      fn.fn()
+    })
+
+}
 
 
 // TODO: 模块依赖 多重依赖 模块的执行机制
